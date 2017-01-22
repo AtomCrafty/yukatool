a36ed445da9d25363ca8b24eb303f307ef636085
AtomCrafty <atomcrafty@frucost.net>
2017-01-22 23:55:27 +0100

    YukaRaw file type and factory

---
+++ src/yukatool/Data/Factory/RawFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yuka.Data.Factory {
+	class RawFactory : FileFactory<YukaRaw> {
+		public override YukaRaw FromBinary(Stream s) {
+			using(MemoryStream ms = new MemoryStream()) {
+				s.CopyTo(ms);
+				return new YukaRaw(ms.ToArray());
+			}
+		}
+
+		public override long ToBinary(YukaRaw data, Stream s) {
+			using(MemoryStream ms = new MemoryStream(data.data)) {
+				ms.WriteTo(s);
+				return ms.Position;
+			}
+		}
+
+		public override YukaRaw FromSource(string filename) {
+			return new YukaRaw(File.ReadAllBytes(filename));
+		}
+
+		public override void ToSource(YukaRaw data, string filename) {
+			File.WriteAllBytes(filename, data.data);
+		}
+	}
+}
---
+++ src/yukatool/Data/YukaRaw.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yuka.Data {
+	class YukaRaw : YukaFile {
+		public byte[] data;
+
+		public YukaRaw(byte[] data) {
+			this.data = data;
+		}
+	}
+}
